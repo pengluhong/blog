3403bc8ce27a351a8f44c00ac39f38b0cf7619ff
pengluhong <pengluhong@yeah.net>
2017-09-07 17:36:45 +0800

    修改

--- js/ng.js
+++ js/ng.js
@@ -1,44 +1,45 @@
 "use strict";
 var app = angular.module('myApp', ['ui.router']);
 app.config(['$urlMatcherFactoryProvider', '$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($urlMatcherFactoryProvider, $stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
-		$httpProvider.defaults.withCredentials = true;
-		$urlRouterProvider.otherwise("/");
-		$urlMatcherFactoryProvider.caseInsensitive(true);
-		var route = [{
-				name: 'Main',
-				url: '/',
-				abstract: true,
-				templateUrl: "A-Study/app/blog/layouts/main.html"
-			},
-			{
-				name: 'Main.news',
-				url: '',
-				templateUrl: "A-Study/app/blog/views/news.html"
-			},
-			{
-				name: 'Main.css',
-				url: 'css',
-				templateUrl: "A-Study/app/blog/views/html-css/css.html"
-			},
-			{
-				name: 'Main.jquery',
-				url: 'jquery',
-				templateUrl: "A-Study/app/blog/views/jquery/jquery.html"
-			},
-			{
-				name: 'Main.ng1',
-				url: 'ng01',
-				templateUrl: "A-Study/app/blog/views/angular/ng1.html"
-			}
-		];
-		angular.forEach(route, function(obj) {
-			$stateProvider.state(obj);
-		});
-		$locationProvider.html5Mode(true);
-	}]);
+	$locationProvider.hashPrefix('');
+	$httpProvider.defaults.withCredentials = true;
+	$urlRouterProvider.otherwise("/");
+	$urlMatcherFactoryProvider.caseInsensitive(true);
+	var route = [{
+			name: 'Main',
+			url: '/',
+			abstract: true,
+			templateUrl: "layouts/main.html"
+		},
+		{
+			name: 'Main.news',
+			url: '',
+			templateUrl: "views/news.html"
+		},
+		{
+			name: 'Main.css',
+			url: 'css',
+			templateUrl: "views/html-css/css.html"
+		},
+		{
+			name: 'Main.jquery',
+			url: 'jquery',
+			templateUrl: "views/jquery/jquery.html"
+		},
+		{
+			name: 'Main.ng1',
+			url: 'ng01',
+			templateUrl: "views/angular/ng1.html"
+		}
+	];
+	angular.forEach(route, function(obj) {
+		$stateProvider.state(obj);
+	});
+	//$locationProvider.html5Mode(true);
+}]);
 
 app.controller('myController', ['$scope', '$state', function($scope, $state) {
-	$scope.Cut = function(){
+	$scope.Cut = function() {
 		console.log($state.current);
 	}
 }]);
