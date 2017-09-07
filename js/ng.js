ab8bfa059ed42d9637ef5a195ae587fa4a770be3
pengluhong <pengluhong@yeah.net>
2017-09-07 17:26:59 +0800

    大修改

--- js/ng.js
+++ js/ng.js
@@ -1,22 +1,44 @@
 "use strict";
-var app = angular.module('myApp', ['ngRoute']);
-//这是因为Angular 1.6 版本更新后对路由做的处理，要加上$locationProvider.hashPrefix('');这样才可以和以前版本一样正常使用
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-	$locationProvider.hashPrefix('');
-	$routeProvider
-		.when('/news', {
-			templateUrl: '../blog/views/news.html'
-		})
-		.when('/css', {
-			templateUrl: '../blog/views/html-css/css.html'
-		})
-		.when('/jquery', {
-			templateUrl: '../blog/views/jquery/jquery.html'
-		})
-		.when('/ng01', {
-			templateUrl: '../blog/views/angular/ng1.html'
-		})
-		.otherwise({
-			redirectTo: '/news'
+var app = angular.module('myApp', ['ui.router']);
+app.config(['$urlMatcherFactoryProvider', '$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($urlMatcherFactoryProvider, $stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+		$httpProvider.defaults.withCredentials = true;
+		$urlRouterProvider.otherwise("/");
+		$urlMatcherFactoryProvider.caseInsensitive(true);
+		var route = [{
+				name: 'Main',
+				url: '/',
+				abstract: true,
+				templateUrl: "A-Study/app/blog/layouts/main.html"
+			},
+			{
+				name: 'Main.news',
+				url: '',
+				templateUrl: "A-Study/app/blog/views/news.html"
+			},
+			{
+				name: 'Main.css',
+				url: 'css',
+				templateUrl: "A-Study/app/blog/views/html-css/css.html"
+			},
+			{
+				name: 'Main.jquery',
+				url: 'jquery',
+				templateUrl: "A-Study/app/blog/views/jquery/jquery.html"
+			},
+			{
+				name: 'Main.ng1',
+				url: 'ng01',
+				templateUrl: "A-Study/app/blog/views/angular/ng1.html"
+			}
+		];
+		angular.forEach(route, function(obj) {
+			$stateProvider.state(obj);
 		});
+		$locationProvider.html5Mode(true);
+	}]);
+
+app.controller('myController', ['$scope', '$state', function($scope, $state) {
+	$scope.Cut = function(){
+		console.log($state.current);
+	}
 }]);
